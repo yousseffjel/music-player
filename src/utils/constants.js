055a25270547b1e6eb300ac7bffec476378ffd1d
Youssef Fjel <youssef.fjel-etu@etu.univh2c.ma>
2024-09-29 19:13:11 +0100

    add constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+// src/utils/constants.js
+
+const CORS_PROXY_PROXY = "https://cors-anywhere.herokuapp.com/";
+const URL_API_DEEZER = "https://api.deezer.com/track/";
+const URL_SEARCH_API = "https://api.deezer.com/search?q=";
+
+export const endPoints = {
+  CORS_PROXY_PROXY,
+  URL_API_DEEZER,
+  URL_SEARCH_API,
+};
