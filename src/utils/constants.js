bbfaba3e9bb44726be19c2cd208b9646831a647e
Youssef Fjel <youssef.fjel-etu@etu.univh2c.ma>
2024-09-30 12:19:40 +0100

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // src/utils/constants.js
 
-const URL_API_DEEZER = process.env.REACT_APP_API_URL || "https://api.deezer.com/track/";
-const URL_SEARCH_API = process.env.REACT_APP_SEARCH_API || "https://api.deezer.com/search?q=";
+const URL_API_DEEZER = process.env.REACT_APP_API_URL || "/api/track/";
+const URL_SEARCH_API = process.env.REACT_APP_SEARCH_API || "/api/search?q=";
 
 export const endPoints = {
   URL_API_DEEZER,
