aed44837064f9ae08d3b409eef4e87874edd1015
Youssef Fjel <youssef.fjel-etu@etu.univh2c.ma>
2024-10-08 14:25:00 +0100

    add commits to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // Specify the files Tailwind should scan for class names
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {},  // No custom theme extensions at the moment
   },
-  plugins: [],
+  plugins: [],  // No additional Tailwind plugins used
 };
